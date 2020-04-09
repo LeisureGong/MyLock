db92532c763b4075a382eb9c7c2b109c84ddd14d
gonglei <gonglei@encdata.cn>
2020-04-09 14:08:58 +0800

    更新

---
+++ src/com/leisure/DeakLockDemo.java
@@ -0,0 +1,8 @@
+package com.leisure;
+
+/**
+ * @author gonglei
+ * @date 2020/4/9 14:08
+ */
+public class DeakLockDemo {
+}
---
+++ src/com/leisure/LockTest.java
@@ -0,0 +1,32 @@
+package com.leisure;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author gonglei
+ * @date 2020/4/9 10:06
+ */
+public class LockTest {
+
+    //悲观锁
+	public synchronized void testMethod(){
+	}
+
+	private ReentrantLock lock = new ReentrantLock();
+	public void modifyPublicResources(){
+		lock.lock();
+		//操作同步资源
+		lock.unlock();
+	}
+
+	//乐观锁
+	private AtomicInteger atomicInteger = new AtomicInteger();
+	public void increment(){
+		atomicInteger.incrementAndGet();
+	}
+	public static void main(String[] args){
+		System.out.println();
+	}
+
+}
