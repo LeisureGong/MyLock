d9bffae79cedbeca2eee8a8b25a2e9ea2533b6d6
gonglei <gonglei@encdata.cn>
2020-04-10 13:56:33 +0800

    对于通知方： 要获得对象的锁，改变条件以及通知所有等待在对象上的线程。

--- src/com/leisure/WaitNotify.java
+++ src/com/leisure/WaitNotify.java
@@ -15,13 +15,25 @@ public class WaitNotify {
 	static boolean flag = true;
 	static Object lock = new Object();
 
+	/**
+	* 等待方需要遵守的规则：
+	* 1)获取对象到的锁
+	 * 2)  如果条件不满足，那么调用对象的wait()方法，被通知后要检查条件
+	 * 3) 条件满足则执行对应的逻辑
+	*
+	* @date 2020/4/10
+	*/
 	static class Wait implements Runnable{
 		@Override
 		public void run(){
 			synchronized ((lock)){
 				while(flag){
+					System.out.println(Thread.currentThread() + " flag is true.wait @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
 					try{
 						System.out.println(Thread.currentThread() + " flag is true.wait @ " + new SimpleDateFormat("HH:mm:ss").format(new Date()));
+						/**
+						* 调用wait()方法后，会释放对象的锁
+						*/
 						lock.wait();
 					}catch (InterruptedException e){
 
@@ -33,6 +45,13 @@ public class WaitNotify {
 		}
 	}
 
+	/**
+	* 通知方遵循的规则：
+	 * 1)获取对象的锁
+	 * 2)改变条件
+	 * 3)通知所有等待在对象上的线程
+	*
+	*/
 	static class Notify implements Runnable{
 		@Override
 		public void run(){
