41ff7c51c769b5e8936ed4a9113108ba32a4a85d
gonglei <gonglei@encdata.cn>
2020-04-17 16:14:37 +0800

    队列同步器

---
+++ src/com/leisure/chap5/Test.java
@@ -0,0 +1,11 @@
+package com.leisure.chap5;
+
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+
+/**
+ * @author gonglei
+ * @date 2020/4/17 15:50
+ */
+public class Test {
+
+}
---
+++ src/com/leisure/chap5/TwinsLockTest.java
@@ -0,0 +1,10 @@
+package com.leisure.chap5;
+
+/**
+ * @author gonglei
+ * @date 2020/4/17 16:11
+ */
+public class TwinsLockTest {
+
+
+}
